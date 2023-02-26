9d8293d75e2182979d973f31ce2848939be73af1
guanrenyang <renyangguan@outlook.com>
2023-02-27 02:02:22 +0800

    use tir to rewrite `coowrite`

--- tw_auto-scheduler/extern_func.py
+++ tw_auto-scheduler/extern_func.py
@@ -1,4 +1,8 @@
 import tvm
+from tvm.ir.module import IRModule
+from tvm.script import ir as I
+from tvm.script import tir as T
+import numpy as np
 from tvm import te
 
 def insert_row_to_C_kernel(M, N, tile_size, block_num, dtype='float32'):
@@ -28,15 +32,14 @@ def insert_row_to_C_kernel(M, N, tile_size, block_num, dtype='float32'):
         #     with irb.for_range(0, M, kind='serial', name='m') as m:
         #         out_ptr[n * M + m] = tvm.tir.generic.cast(0, data.dtype)
 
-        with irb.for_range(0, block_num, kind='parallel', name='bn') as bn:
-            with irb.for_range(0, tile_size, kind='parallel', name='ts') as ts:
-                with irb.for_range(0, M, kind='parallel', name='col') as col:
+        with irb.for_range(0, block_num, kind='serial', name='bn') as bn:
+            with irb.for_range(0, tile_size, kind='serial', name='ts') as ts:
+                with irb.for_range(0, M, kind='serial', name='col') as col:
                     '''Now, pointer in TensorIR can be accessed as a un-flattened tensor'''
-                    with irb.new_scope():
-                        out_ptr[(N_ori_per_block * bn + mask_n_ptr[bn, ts]), col] += data_ptr[bn, ts, col]
+                    out_ptr[(N_ori_per_block * bn + mask_n_ptr[bn, ts]), col] += data_ptr[bn, ts, col]
         sch = irb.get()
-        print(type(sch))
-        return schs
+        # print(sch)
+        return sch
         
     C_transposed = te.extern((N, M),
                             [C_transposed_skipped, mask_n],
@@ -55,5 +58,41 @@ def insert_row_to_C_kernel(M, N, tile_size, block_num, dtype='float32'):
 
     return s, [C_transposed, C_transposed_skipped, mask_n]
 
-sch, args = insert_row_to_C_kernel(1024, 1024, 128, 8, 'float32')
-# print(tvm.lower(sch, args, simple_mode=True).script()) 
+
+
+def insert_row_to_C_kernel_script(M, N, tile_size, block_num, dtype='float32'):
+    N_ori_per_block = N // block_num
+    @I.ir_module
+    class Module:
+        @T.prim_func
+        def main(C_transposed_h: T.handle, C_transposed_skipped_h: T.handle, mask_n_h: T.handle):
+            T.func_attr({"from_legacy_te_schedule": True, "global_symbol": "main", "tir.noalias": True})
+            C_transposed = T.match_buffer(C_transposed_h, (N, M), dtype)
+            C_transposed_skipped = T.match_buffer(C_transposed_skipped_h, (block_num, tile_size, M), dtype)
+            mask_n = T.match_buffer(mask_n_h, (block_num, tile_size), 'int32')
+            for bn in range(block_num):
+                for ts in range(tile_size):
+                    for col in range(M):
+                        with T.block('B'):
+                                C_transposed[(N_ori_per_block * bn + mask_n[bn, ts]), col] += C_transposed_skipped[bn, ts, col]
+
+    ir_module = Module
+    sch = tvm.tir.Schedule(ir_module)
+    
+    block_b = sch.get_block('B')
+    (bn, ts, m) = sch.get_loops(block_b)
+    m_o, m_i = sch.split(m, factors=[M//32, 32])
+    
+    sch.bind(bn, 'blockIdx.z')
+    sch.bind(ts, 'threadIdx.y')
+    sch.bind(m_o, 'blockIdx.x')
+    sch.bind(m_i, 'threadIdx.x')
+
+    print(ir_module.script())
+insert_row_to_C_kernel_script(2048, 1024, 128, 8, 'float32')
+# sch, args = insert_row_to_C_kernel(2048, 1024, 128, 8, 'float32')
+# ir_module = tvm.lower(sch, args, simple_mode=True)
+# sch = tvm.tir.Schedule(ir_module)
+
+
+# print(ir_module.script()) 
