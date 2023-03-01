f90236f16b78b60ea5762047cfff9fc2023b3856
guanrenyang <renyangguan@outlook.com>
2023-03-02 01:49:02 +0800

    ready to do experiments

--- tw_auto-scheduler/extern_func.py
+++ tw_auto-scheduler/extern_func.py
@@ -5,7 +5,7 @@ from tvm.script import tir as T
 import numpy as np
 from tvm import te
 
-def insert_row_to_C_kernel(M, N, tile_size, block_num, dtype='float32'):
+def insert_row_to_C_kernel_te(M, N, tile_size, block_num, dtype='float32'):
     C_transposed_skipped = te.placeholder((block_num, tile_size, M), name='C_transposed_skipped', dtype=dtype)
     mask_n = te.placeholder((block_num, tile_size), name='mask_n', dtype='int32') 
     def write_C_to_sparse(data, mask_n, out):
@@ -60,39 +60,84 @@ def insert_row_to_C_kernel(M, N, tile_size, block_num, dtype='float32'):
 
 
 
-def insert_row_to_C_kernel_script(M, N, tile_size, block_num, dtype='float32'):
+def is_power_of_two(n: int):
+    if n&(n-1)==0:
+        return True
+    return False
+def insert_row_to_C_kernel_tir(M, N, tile_size, block_num, dtype='float32', target_name='cpu'):
     N_ori_per_block = N // block_num
+    cuda = True if target_name in ['gpu', 'cuda'] else False
+
     @I.ir_module
-    class Module:
+    class CPUModule:
         @T.prim_func
         def main(C_transposed_h: T.handle, C_transposed_skipped_h: T.handle, mask_n_h: T.handle):
-            T.func_attr({"from_legacy_te_schedule": True, "global_symbol": "main", "tir.noalias": True})
+            T.func_attr({"global_symbol": "main", "tir.noalias": True})
             C_transposed = T.match_buffer(C_transposed_h, (N, M), dtype)
             C_transposed_skipped = T.match_buffer(C_transposed_skipped_h, (block_num, tile_size, M), dtype)
             mask_n = T.match_buffer(mask_n_h, (block_num, tile_size), 'int32')
-            for bn in range(block_num):
-                for ts in range(tile_size):
-                    for col in range(M):
+
+            # for bn, ts, col in T.grid(block_num, tile_size, M):
+            for bn in T.parallel(block_num):
+                for col in T.vectorized(M):
+                    for ts in T.unroll(tile_size):
                         with T.block('B'):
-                                C_transposed[(N_ori_per_block * bn + mask_n[bn, ts]), col] += C_transposed_skipped[bn, ts, col]
+                            with T.init():
+                                C_transposed[(N_ori_per_block * bn + mask_n[bn, ts]), col] = T.float32(0)
+                            C_transposed[(N_ori_per_block * bn + mask_n[bn, ts]), col] += C_transposed_skipped[bn, ts, col]
+    @I.ir_module
+    class GPUModule:
+        @T.prim_func
+        def main(C_transposed_h: T.handle, C_transposed_skipped_h: T.handle, mask_n_h: T.handle):
+            T.func_attr({"global_symbol": "main", "tir.noalias": True})
+            C_transposed = T.match_buffer(C_transposed_h, (N, M), dtype)
+            C_transposed_skipped = T.match_buffer(C_transposed_skipped_h, (block_num, tile_size, M), dtype)
+            mask_n = T.match_buffer(mask_n_h, (block_num, tile_size), 'int32')
 
-    ir_module = Module
-    sch = tvm.tir.Schedule(ir_module)
-    
-    block_b = sch.get_block('B')
-    (bn, ts, m) = sch.get_loops(block_b)
-    m_o, m_i = sch.split(m, factors=[M//32, 32])
-    
-    sch.bind(bn, 'blockIdx.z')
-    sch.bind(ts, 'threadIdx.y')
-    sch.bind(m_o, 'blockIdx.x')
-    sch.bind(m_i, 'threadIdx.x')
+            for bn, ts, col in T.grid(block_num, tile_size, M):
+                with T.block('B'):
+                    with T.init():
+                        C_transposed[(N_ori_per_block * bn + mask_n[bn, ts]), col] = T.float32(0)
+                    C_transposed[(N_ori_per_block * bn + mask_n[bn, ts]), col] += C_transposed_skipped[bn, ts, col]
+    if cuda is False:
+        ir_module = CPUModule
+        sch = tvm.tir.Schedule(ir_module)
+    else:
+        ir_module = GPUModule
+        sch = tvm.tir.Schedule(ir_module)
 
-    print(ir_module.script())
-insert_row_to_C_kernel_script(2048, 1024, 128, 8, 'float32')
+        # block_a = sch.get_block('A')
+        # (n, m) = sch.get_loops(block_a)
+        # no, ni = sch.split(n, [N//32, 32])
+        # mo, mi = sch.split(m, [M//32, 32])
+        # sch.bind(no, 'blockIdx.x')
+        # sch.bind(mo, 'blockIdx.y')
+        # sch.bind(ni, 'threadIdx.x')
+        # sch.bind(mi, 'threadIdx.y')
+        
+        block_b = sch.get_block('B')
+        (bn, ts, m) = sch.get_loops(block_b)
+
+        assert is_power_of_two(tile_size), '`tile_size` must be power of 2 but now it is not.'
+        assert tile_size<=1024, '`tile_size` must be less than or equal to 1024 but now it is not.'
+        assert M>=1024, '`M` must be greater than or equal to 1024 but now it is not.'
+
+        mo, mi = sch.split(m, factors=[M//(1024//tile_size), 1024//tile_size])
+        sch.reorder(mo, bn, ts, mi)
+        
+        sch.bind(mo, 'blockIdx.y')
+        sch.bind(bn, 'blockIdx.x')
+        sch.bind(ts, 'threadIdx.y')
+        sch.bind(mi, 'threadIdx.x')
+
+    
+    func = tvm.build(sch.mod, target=('cuda' if cuda else 'llvm'))
+    return func
+
+
 # sch, args = insert_row_to_C_kernel(2048, 1024, 128, 8, 'float32')
 # ir_module = tvm.lower(sch, args, simple_mode=True)
-# sch = tvm.tir.Schedule(ir_module)
-
-
 # print(ir_module.script()) 
+
+
+
--- tw_auto-scheduler/tw_auto_scheduler.py
+++ tw_auto-scheduler/tw_auto_scheduler.py
@@ -4,33 +4,34 @@ from tvm import te, auto_scheduler
 import numpy as np
 import random
 
-from extern_func import insert_row_to_C_kernel
+from extern_func import insert_row_to_C_kernel_tir  
 
-# N_pruned is the number of remaining entries in N dimension
-# TODO: change K_pruned to a layer-wise configuration
-big_test = True
-dtype = 'float32'
-if big_test:
-    '''test for accuracy'''
-    M = 1024
-    N = 1024
-    K = 1024
-    tile_size = 32
-    K_pruned_max = 512
-    N_pruned_global = 512
-else:
-    '''test for visualization'''
-    M = 16
-    N = 16
-    K = 16
-    tile_size =  2
-    K_pruned_max = 8
-    N_pruned_global = 8
-block_num = (N_pruned_global + tile_size - 1)//tile_size
-N_ori_per_block = N // block_num
-
+is_resume = False
+is_tune = True
+ansor_num_trials = 1000
+ansor_verbose = 1
+# # N_pruned is the number of remaining entries in N dimension
+# # TODO: change K_pruned to a layer-wise configuration
+# big_test = True
+# dtype = 'float32'
+# if big_test:
+#     '''test for accuracy'''
+#     M = 1024
+#     N = 1024
+#     K = 1024
+#     tile_size = 32
+#     K_pruned_max = 512
+#     N_pruned_global = 1024
+# else:
+#     '''test for visualization'''
+#     M = 16
+#     N = 16
+#     K = 16
+#     tile_size =  2
+#     K_pruned_max = 8
+#     N_pruned_global = 8
 @auto_scheduler.register_workload
-def tw_kernel(M, K, K_pruned_max, tile_size, block_num, dtype='float32'):
+def tw_kernel(M, K, K_pruned_max, tile_size, block_num, dtype):
 
     A_transposed = te.placeholder((K, M), name='A_transposed', dtype=dtype)
     B_transposed_packed = te.placeholder((block_num, tile_size, K_pruned_max), name='B_transposed_packed', dtype=dtype)
@@ -44,116 +45,121 @@ def tw_kernel(M, K, K_pruned_max, tile_size, block_num, dtype='float32'):
 
     return [C_transposed_skipped, A_transposed, B_transposed_packed, mask_k]
     
-# generate random mask_k for Ansor
-mask_k_test = np.zeros((block_num, K_pruned_max)).astype('int32')
-for row in range(block_num):
-    mask_k_test[row, :] = np.random.choice(K, K_pruned_max, replace=False)
-mask_k_test.sort(axis=1)
 
-# create search task
-target = tvm.target.cuda(arch='sm_70')
-device = tvm.device(target.get_target_device_type(), 0)
+def get_execution_time(M, N, K, tile_size, K_pruned_max, N_pruned_global, dtype, target_name:str):
+    block_num = (N_pruned_global + tile_size - 1)//tile_size
+    N_ori_per_block = N // block_num
+        
 
-task = tvm.auto_scheduler.SearchTask(func=tw_kernel, args=(M, K, K_pruned_max, tile_size, block_num, dtype), target=target, 
-                                     task_inputs={'mask_k': tvm.runtime.ndarray.array(mask_k_test)})
+    # generate random mask_k for Ansor
+    mask_k_test = np.zeros((block_num, K_pruned_max)).astype('int32')
+    for row in range(block_num):
+        mask_k_test[row, :] = np.random.choice(K, K_pruned_max, replace=False)
+    mask_k_test.sort(axis=1)
 
-# Inspect the computational graph
-print("Computational DAG:")
-print(task.compute_dag)
+    # create search task
+    if target_name in ['cuda', 'gpu']:
+        target = tvm.target.cuda(arch='sm_70')
+        device = tvm.device(target.get_target_device_type(), 0)
+    else :
+        target = tvm.target.Target(target="llvm", host="llvm")
+        device = tvm.device(target.kind.name, 0)
 
-# Custom Sketch
-def meet_condition_func(search_policy, state, stage_id):
-    state = auto_scheduler.loop_state.State(state, search_policy.search_task.compute_dag)
-    print(stage_id, state.stages[stage_id].op.tag)
-    if state.stages[stage_id].op.tag in [
-        "A_transposed_skipped",
-    ]:
-        return auto_scheduler.PreloadCustomSketchRule.APPLY_AND_SKIP_REST
+    task = tvm.auto_scheduler.SearchTask(func=tw_kernel, args=(M, K, K_pruned_max, tile_size, block_num, dtype), target=target, 
+                                        task_inputs={'mask_k': tvm.runtime.ndarray.array(mask_k_test)}, task_inputs_overwrite=True)
+
+    # Inspect the computational graph
+    # print("Computational DAG:")
+    # print(task.compute_dag)
+
+    # Set parameters for auto_scheduler
+    log_file = "./ansor_outputs/tw_{M}_{N}_{K}_{tile_size}_{K_pruned_max}_{N_pruned_global}_{dtype}_{target_name}_trails{trails}.json".format(M=M, N=N, K=K, tile_size=tile_size, K_pruned_max=K_pruned_max, N_pruned_global=N_pruned_global, dtype=dtype, target_name=target_name, trails=ansor_num_trials)
+    tune_option = auto_scheduler.TuningOptions(
+        num_measure_trials=ansor_num_trials,
+        measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
+        verbose=ansor_verbose,
+    )
+    cost_model = auto_scheduler.XGBModel()
+    if is_resume:
+        cost_model.update_from_file(log_file)
+
+    if is_resume:
+        search_policy = auto_scheduler.SketchPolicy(
+            task,
+            program_cost_model=cost_model,
+            init_search_callbacks=[
+                auto_scheduler.PreloadMeasuredStates(log_file)
+            ],
+        )
     else:
-        return auto_scheduler.PreloadCustomSketchRule.PASS
+        search_policy = auto_scheduler.SketchPolicy(
+            task,
+            program_cost_model=cost_model,
+        )
+
+    # Run auto-tuning (search)
+    task.tune(tune_option, search_policy=search_policy)
+
+    # Apply the best schedule
+    sch_tw_mainbody, args_tw_mainbody = task.apply_best(log_file)
+
+    # Check correctness and evaluate performance
+    tw_mainbody = tvm.build(sch_tw_mainbody, args_tw_mainbody, target)
+    tw_coowrite_C = insert_row_to_C_kernel_tir(M, N, tile_size, block_num, dtype, target_name) # Get the extern function `coowrite`
+
+    '''generate data for testing'''
+    # generate mask_n
+    mask_n_test = np.zeros((block_num, tile_size)).astype('int32')
+    for row in range(block_num):
+        mask_n_test[row, :] = np.random.choice(N_ori_per_block, tile_size, replace=False)
+    mask_n_test.sort(axis=1)
+
+    A_transposed_test = np.random.random((K, M)).astype(dtype)
+    B_transposed_test = np.random.random((N, K)).astype(dtype)
+    C_transposed_skipped_test = np.zeros((block_num, tile_size, M)).astype(dtype)
+    C_transposed_test = np.zeros((N, M)).astype(dtype)
 
 
-def apply_func(search_policy, state, stage_id):
-    ret = []
+    # apply mask to B_transposed
+    B_transposed_pruned = np.zeros((N, K)).astype(dtype)
+    for bn in range(block_num):
+        # print(bn)
+        for ts in range(tile_size):
+            for k in range(K_pruned_max):
+                B_transposed_pruned[(bn*N_ori_per_block+mask_n_test[bn, ts]), mask_k_test[bn, k]] = B_transposed_test[(bn*N_ori_per_block+mask_n_test[bn, ts]), mask_k_test[bn, k]]
 
-    return ret
-
-# Set parameters for auto_scheduler
-log_file = "tw.json"
-tune_option = auto_scheduler.TuningOptions(
-    num_measure_trials=10,
-    measure_callbacks=[auto_scheduler.RecordToFile(log_file)],
-    verbose=2,
-)
-
-search_policy = auto_scheduler.SketchPolicy(
-    task,
-    program_cost_model=auto_scheduler.XGBModel(),
-    init_search_callbacks=[
-        auto_scheduler.PreloadCustomSketchRule(meet_condition_func, apply_func)
-    ],
-)
-
-# Run auto-tuning (search)
-task.tune(tune_option)
-
-# Apply the best schedule
-sch_tw_mainbody, args_tw_mainbody = task.apply_best(log_file)
-# Get the extern function `coowrite`
-sch_tw_coowrite_C, args_tw_coowrite_C = insert_row_to_C_kernel(M, N, tile_size, block_num, dtype)
-
-# # Inspecting the Optimized Schedule
-# print("Lowered TIR of tw mainbody:")
-# print(tvm.lower(sch_tw_mainbody, args_tw_mainbody, simple_mode=True))
-# print("Lowered TIR of coowrite")
-# print(tvm.lower(sch_tw_coowrite_C, args_tw_coowrite_C, simple_mode=True))
-
-# Check correctness and evaluate performance
-tw_mainbody = tvm.build(sch_tw_mainbody, args_tw_mainbody, target)
-tw_coowrite_C = tvm.build(sch_tw_coowrite_C, args_tw_coowrite_C, target)
-
-'''generate data for testing'''
-# generate mask_n
-mask_n_test = np.zeros((block_num, tile_size)).astype('int32')
-for row in range(block_num):
-    mask_n_test[row, :] = np.random.choice(N_ori_per_block, tile_size, replace=False)
-mask_n_test.sort(axis=1)
-
-A_transposed_test = np.random.random((K, M)).astype(dtype)
-B_transposed_test = np.random.random((N, K)).astype(dtype)
-C_transposed_skipped_test = np.zeros((block_num, tile_size, M)).astype(dtype)
-C_transposed_test = np.zeros((N, M)).astype(dtype)
+    # transpose B to B_packed
+    B_transposed_packed_test = np.zeros((block_num, tile_size, K_pruned_max)).astype(dtype)
+    for bn in range(block_num):
+        # print(bn)
+        for ts in range(tile_size):
+            for k in range(K_pruned_max):
+                B_transposed_packed_test[bn, ts, k] = B_transposed_pruned[(bn*N_ori_per_block+mask_n_test[bn, ts]), mask_k_test[bn, k]]
 
 
-# apply mask to B_transposed
-B_transposed_pruned = np.zeros((N, K)).astype(dtype)
-for bn in range(block_num):
-    # print(bn)
-    for ts in range(tile_size):
-        for k in range(K_pruned_max):
-            B_transposed_pruned[(bn*N_ori_per_block+mask_n_test[bn, ts]), mask_k_test[bn, k]] = B_transposed_test[(bn*N_ori_per_block+mask_n_test[bn, ts]), mask_k_test[bn, k]]
+    A_transposed_test = tvm.nd.array(A_transposed_test, device)
+    B_transposed_packed_test = tvm.nd.array(B_transposed_packed_test, device)
+    C_transposed_skipped_test = tvm.nd.array(C_transposed_skipped_test, device) # a temporary buffer
+    C_transposed_test = tvm.nd.array(C_transposed_test, device)
+    mask_k_test = tvm.nd.array(mask_k_test, device)
+    mask_n_test = tvm.nd.array(mask_n_test, device)
 
-# transpose B to B_packed
-B_transposed_packed_test = np.zeros((block_num, tile_size, K_pruned_max)).astype(dtype)
-for bn in range(block_num):
-    # print(bn)
-    for ts in range(tile_size):
-        for k in range(K_pruned_max):
-            B_transposed_packed_test[bn, ts, k] = B_transposed_pruned[(bn*N_ori_per_block+mask_n_test[bn, ts]), mask_k_test[bn, k]]
+    # run the kernel
+    tw_mainbody(C_transposed_skipped_test, A_transposed_test, B_transposed_packed_test, mask_k_test)
+    tw_coowrite_C(C_transposed_test, C_transposed_skipped_test, mask_n_test)
 
+    # check results
+    tvm.testing.assert_allclose(C_transposed_test.numpy().T, A_transposed_test.numpy().T @ B_transposed_pruned.T, 1e-1)
 
-A_transposed_test = tvm.nd.array(A_transposed_test, device)
-B_transposed_packed_test = tvm.nd.array(B_transposed_packed_test, device)
-C_transposed_skipped_test = tvm.nd.array(C_transposed_skipped_test, device) # a temporary buffer
-C_transposed_test = tvm.nd.array(C_transposed_test, device)
-mask_k_test = tvm.nd.array(mask_k_test, device)
-mask_n_test = tvm.nd.array(mask_n_test, device)
+    # Evaluate execution time.
+    tw_mainbody_evaluator = tw_mainbody.time_evaluator(tw_mainbody.entry_name, device, min_repeat_ms=500)
+    tw_mainbody_execution_time = np.median(tw_mainbody_evaluator(C_transposed_skipped_test, A_transposed_test, B_transposed_packed_test, mask_k_test).results) * 1000
+    
+    tw_coowrite_C_evaluator = tw_coowrite_C.time_evaluator(tw_coowrite_C.entry_name, device, min_repeat_ms=500)
+    tw_coowrite_C_execution_time = np.median(tw_coowrite_C_evaluator(C_transposed_test, C_transposed_skipped_test, mask_n_test).results) * 1000
 
-# run the kernel
-tw_mainbody(C_transposed_skipped_test, A_transposed_test, B_transposed_packed_test, mask_k_test)
-tw_coowrite_C(C_transposed_test, C_transposed_skipped_test, mask_n_test)
+    print("\tExecution time of tw_mainbody: %.3f ms" % (tw_mainbody_execution_time))
+    print("\tExecution time of tw_coowrite_C: %.3f ms" % (tw_coowrite_C_execution_time))
 
-tvm.testing.assert_allclose(C_transposed_test.numpy().T, A_transposed_test.numpy().T @ B_transposed_pruned.T, 1e-1)
-
-
-
+    return tw_mainbody_execution_time, tw_coowrite_C_execution_time
+    # print(tw_mainbody.imported_modules[0].get_source())
